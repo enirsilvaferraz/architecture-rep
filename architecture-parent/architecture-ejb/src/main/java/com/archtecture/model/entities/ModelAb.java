1204c00643c9277db9838d9eebe962149b8be35e
enirsilvaferraz <Enir@W7430>
2015-05-30 16:18:45 -0300

    Configuração Anterior

--- architecture-parent/architecture-ejb/src/main/java/com/archtecture/model/entities/ModelAb.java
+++ architecture-parent/architecture-ejb/src/main/java/com/archtecture/model/entities/ModelAb.java
@@ -2,13 +2,19 @@ package com.archtecture.model.entities;
 
 import java.io.Serializable;
 
-@SuppressWarnings("serial")
 public abstract class ModelAb implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4601241836318767159L;
 
 	@Deprecated
 	public abstract String[] getAtributosOrdencao();
 
 	public abstract Long getCodigo();
+	
+	public abstract void setCodigo(Long id);
 	
 	public String getLabel(){
 		return toString();
